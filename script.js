0b9ded59eb883cf9ece29719dc4ec969e612e4f7
ListratovAI <listratov06@gmail.com>
2020-02-24 19:13:02 +0300

    Проба

---
+++ script.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const server = new http.Server();
+
+let i = 0;
+server.on("request", (req, res) => {
+  i++;
+  res.end(`Hello world. ${i}`);
+});
+
+const emit = server.emit;
+
+server.emit = (...args) => {
+  console.log(args[0]);
+  return emit.apply(server, args);
+};
+
+server.listen(8000);
